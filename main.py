6e1f0bff149179e76e45dcde9cbe89658ad610c6
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-14 09:39:01 -0500

    a couple basic classes

---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import random
+
+class Die:
+    def __init__(self, sides: list[int], sprite: str):
+        self.sides = sides
+        self.sprite = sprite
+
+    def roll(self) -> int:
+        return random.choice(self.sides)
+    
+    def __str__(self) -> str:
+        return f"{len(self.sides)}-sided die"
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def draw(self):
+        pass
+
+
+class DiceBag:
+    def __init__(self, dice: list[Die], sprite: str):
+        self.contents = {n: [] for n in [4, 6, 8, 10, 12, 20]}
+        self.add_dice(dice)
+
+        self.sprite = sprite
+
+    def add_dice(self, dice):
+        for die in dice:
+            num_sides = len(die.sides)
+            self.contents[num_sides].append(die)
+
+    def pull_dice(self, n_dice: int, n_sides: int) -> list[Die]:
+        dice = []
+        options = self.contents[n_sides]
+        for i in range(n_dice):
+            if len(options) == 0:
+                break
+
+            die = random.choice(options)
+            options.remove(die)
+            dice.append(die)
+
+        return dice
+    
+    def all_dice(self) -> list[Die]:
+        dice = []
+        for n_sided_dice in self.contents.values():
+            dice.extend(n_sided_dice)
+        return dice
+    
+    def size(self) -> int:
+        return len(self.all_dice())
+    
+    def __str__(self) -> str:
+        return f"dice bag containing: {self.all_dice()}"
+
+    def __repr__(self) -> str:
+        return str(self)
+        
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass

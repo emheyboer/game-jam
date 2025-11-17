1bda97ea3d09401d6f50e34f3ae805afb715d329
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-17 18:33:18 -0500

    move screen-specific code to dedicated class

---
+++ src/combatScreen.py
@@ -0,0 +1,200 @@
+import pygame
+
+from sprites import Sprite
+from dice import Die
+from diceBag import DiceBag
+from attack import Attack
+from actors import Actor
+from button import Button
+
+class combatScreen:
+    def __init__(self, screen, width: int, height: int, sprites) -> None:
+        self.screen = screen
+        self.width = width
+        self.height = height
+        self.sprites = sprites
+
+        self.init_player()
+        self.init_boss()
+
+        self.buttons = []
+
+        x_mult = .2
+        for i in range(len(self.player.attacks)):
+            button = Button(
+                f"{self.player.attacks[i]}",
+                (width * x_mult, height * .8),
+                (width * .1, height * .1),
+                sprites['button_attack'],
+                'attack',
+                value=i
+            )
+            self.buttons.append(button)
+            x_mult += .125
+        
+        self.take_turn()
+
+
+    def init_player(self) -> None:
+        player_dice_bag = DiceBag([
+            Die(range(1,21), self.sprites['d20_trans']),
+            Die(range(1, 13), self.sprites['d12_trans']),
+            Die(range(1, 11), self.sprites['d10_trans']),
+            Die(range(1, 9), self.sprites['d8_trans']),
+            Die(range(1, 7), self.sprites['d6_trans']),
+            Die(range(1, 5), self.sprites['d4_trans']),
+            Die(range(1,21), self.sprites['d20_trans']),
+            Die(range(1, 13), self.sprites['d12_trans']),
+            Die(range(1, 11), self.sprites['d10_trans']),
+            Die(range(1, 9), self.sprites['d8_trans']),
+            Die(range(1, 7), self.sprites['d6_trans']),
+            Die(range(1, 5), self.sprites['d4_trans']),
+            Die(range(1,21), self.sprites['d20_trans']),
+            Die(range(1, 13), self.sprites['d12_trans']),
+            Die(range(1, 11), self.sprites['d10_trans']),
+            Die(range(1, 9), self.sprites['d8_trans']),
+            Die(range(1, 7), self.sprites['d6_trans']),
+            Die(range(1, 5), self.sprites['d4_trans']),
+        ], self.sprites['dice_bag'])
+
+        player_attacks = [
+            Attack([(3, 20)]),
+            Attack([(1, 12)]),
+            Attack([(2, 10)]),
+            Attack([(2, 8)]),
+            Attack([(3, 6)]),
+        ]
+
+        self.player = Actor(
+            'player',
+            self.sprites['art_boss'],
+            self.sprites['dice_box_player'],
+            player_dice_bag,
+            player_attacks,
+        )
+
+    def init_boss(self) -> None:
+        boss_dice_bag = DiceBag([
+            Die(range(1,21), self.sprites['d20_white']),
+            Die(range(1, 13), self.sprites['d12_white']),
+            Die(range(1, 11), self.sprites['d10_white']),
+            Die(range(1, 9), self.sprites['d8_white']),
+            Die(range(1, 7), self.sprites['d6_white']),
+            Die(range(1, 5), self.sprites['d4_white']),
+            Die(range(1,21), self.sprites['d20_white']),
+            Die(range(1, 13), self.sprites['d12_white']),
+            Die(range(1, 11), self.sprites['d10_white']),
+            Die(range(1, 9), self.sprites['d8_white']),
+            Die(range(1, 7), self.sprites['d6_white']),
+            Die(range(1, 5), self.sprites['d4_white']),
+            Die(range(1,21), self.sprites['d20_white']),
+            Die(range(1, 13), self.sprites['d12_white']),
+            Die(range(1, 11), self.sprites['d10_white']),
+            Die(range(1, 9), self.sprites['d8_white']),
+            Die(range(1, 7), self.sprites['d6_white']),
+            Die(range(1, 5), self.sprites['d4_white']),
+        ], self.sprites['dice_bag'])
+        self.boss = Actor(
+            'boss',
+            self.sprites['art_boss'],
+            self.sprites['dice_box_boss'],
+            boss_dice_bag,
+            [
+                Attack([(2, 8), (3, 6)]),
+                Attack([(3, 20)]),
+            ],
+        )
+
+    def draw(self) -> None:
+        width, height = self.width, self.height
+
+        ui_sections = [
+            ((200, 0, 0), (0, 0, width * .2, height * .375), "spooky boss art"),
+            ((200, 0, 0), (width * .8, 0, width * .2, height * .375), "boss dice bag"),
+
+            ((0, 200, 0), (0, height * .375, width * .2, height * .375), "player stats/art?"),
+            ((0, 200, 0), (width * .8, height * .375, width * .2, height * .375), "player dice bag"),
+
+            ((0, 0, 200), (0, height * .75, width * .2, height * .25), "game options?"),
+            ((0, 0, 100), (width * .2, height * .75, width * .6, height * .25), "attack options"),
+            ((0, 0, 200), (width * .8, height * .75, width * .2, height * .25), "game options?"),
+        ]
+
+        font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
+
+        self.boss.dice_box_art.draw(self.screen, (width * .2, 0), size = (width * .6, height * .375),
+                            text=str(self.boss_total))
+        self.player.dice_box_art.draw(self.screen, (width * .2, height * .375), size = (width * .6, height * .375),
+                                text=str(self.player_total))
+
+        for section in ui_sections:
+            color, rect, text = section
+
+            pygame.draw.rect(self.screen, color, rect)
+
+            label = font.render(text, False, (255, 255, 255))
+
+            w, h, _, _ = rect
+            self.screen.blit(label, (w, h))
+
+
+        self.boss.dice_bag.draw(self.screen, (width * .8, height * .475), size = (width * .225, height * .225))
+        self.player.dice_bag.draw(self.screen, (width * .8, height * .1), size = (width * .225, height * .225))
+
+        self.boss.profile_art.draw(self.screen, (0, height * .1), size = (width * .225, height * .225))
+
+        for btn in self.buttons:
+            btn.draw(self.screen)
+
+        self.draw_dice('boss', self.boss_dice)
+        self.draw_dice('player', self.player_dice)
+
+
+    def draw_dice(self, box: str, dice: list[Die]) -> None:
+        """
+        Draw dice in either of the two dice boxes
+        """
+        width, height = self.width, self.height
+
+        if box == 'boss':
+            y = height * .2
+        elif box == 'player':
+            y = height * .575
+
+        count = len(dice)
+        if count <= 5:
+            deltaX = width * .1
+            size = (width * .1, width * .1)
+            deltaY = -height * (.05)
+        else: # adapt sizes & positions to fit in the same space
+            divisor = count / 5
+
+            deltaX = width * .1 / divisor
+            size = (deltaX, deltaX)
+            deltaY = -height * .05 / divisor
+
+        x = width * .25
+        for die in dice:
+            y += deltaY
+            die.draw(self.screen, (x, y), size=size)
+            x += deltaX
+            deltaY *= -1
+
+    def take_turn(self, atkPlayer: int|None = None, atkBoss: int|None = None) -> None:
+        self.player_total, self.player_dice = self.player.roll_attack(atk_index=atkPlayer)
+        self.boss_total, self.boss_dice = self.boss.roll_attack(atk_index=atkBoss)
+
+        # only the winner's dice are re-added to their bag (or both in the case of a tie)
+        if self.player_total >= self.boss_total:
+            self.player.dice_bag.add_dice(self.player_dice)
+        if self.boss_total >= self.player_total:
+            self.boss.dice_bag.add_dice(self.boss_dice)
+    
+    def on_event(self, event) -> None:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos = pygame.mouse.get_pos()
+            for btn in self.buttons:
+                if btn.inside(pos) and btn.kind == 'attack':
+                    self.take_turn(atkPlayer=btn.value)
+                    print(f"player = {self.player.dice_bag.size()}, boss = {self.boss.dice_bag.size()}")
+                    break
--- src/main.py
+++ src/main.py
@@ -7,94 +7,7 @@ from sprites import load_sprites
 from actors import Actor
 from button import Button
 from attack import Attack
-
-
-def draw_dice(screen, width: int, height: int, box: str, dice: list[Die]) -> None:
-    """
-    Draw dice in either of the two dice boxes
-    """
-    if box == 'boss':
-        y = height * .2
-    elif box == 'player':
-        y = height * .575
-
-    count = len(dice)
-    if count <= 5:
-        deltaX = width * .1
-        size = (width * .1, width * .1)
-        deltaY = -height * (.05)
-    else: # adapt sizes & positions to fit in the same space
-        divisor = count / 5
-
-        deltaX = width * .1 / divisor
-        size = (deltaX, deltaX)
-        deltaY = -height * .05 / divisor
-
-    x = width * .25
-    for die in dice:
-        y += deltaY
-        die.draw(screen, (x, y), size=size)
-        x += deltaX
-        deltaY *= -1
-
-
-def take_turn(actorA: Actor, actorB: Actor,
-              atkA: int|None = None, atkB: int|None = None) -> tuple[int, int, list[Die], list[Die]]:
-    (totalA, diceA) = actorA.roll_attack(atk_index=atkA)
-    (totalB, diceB) = actorB.roll_attack(atk_index=atkB)
-
-    # only the winner's dice are re-added to their bag (or both in the case of a tie)
-    if totalA >= totalB:
-        actorA.dice_bag.add_dice(diceA)
-    if totalB >= totalA:
-        actorB.dice_bag.add_dice(diceB)
-    
-
-    return (totalA, totalB, diceA, diceB)
-
-
-def render_combat(screen, width: int, height: int, buttons: list[Button], player: Actor, boss: Actor,
-                  player_total: int, boss_total: int, player_dice: list[Die], boss_dice: list[Die]):
-    ui_sections = [
-        ((200, 0, 0), (0, 0, width * .2, height * .375), "spooky boss art"),
-        ((200, 0, 0), (width * .8, 0, width * .2, height * .375), "boss dice bag"),
-
-        ((0, 200, 0), (0, height * .375, width * .2, height * .375), "player stats/art?"),
-        ((0, 200, 0), (width * .8, height * .375, width * .2, height * .375), "player dice bag"),
-
-        ((0, 0, 200), (0, height * .75, width * .2, height * .25), "game options?"),
-        ((0, 0, 100), (width * .2, height * .75, width * .6, height * .25), "attack options"),
-        ((0, 0, 200), (width * .8, height * .75, width * .2, height * .25), "game options?"),
-    ]
-
-    font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
-
-    boss.dice_box_art.draw(screen, (width * .2, 0), size = (width * .6, height * .375),
-                           text=str(boss_total))
-    player.dice_box_art.draw(screen, (width * .2, height * .375), size = (width * .6, height * .375),
-                             text=str(player_total))
-
-    for section in ui_sections:
-        color, rect, text = section
-
-        pygame.draw.rect(screen, color, rect)
-
-        label = font.render(text, False, (255, 255, 255))
-
-        w, h, _, _ = rect
-        screen.blit(label, (w, h))
-
-
-    boss.dice_bag.draw(screen, (width * .8, height * .475), size = (width * .225, height * .225))
-    player.dice_bag.draw(screen, (width * .8, height * .1), size = (width * .225, height * .225))
-
-    boss.profile_art.draw(screen, (0, height * .1), size = (width * .225, height * .225))
-
-    for btn in buttons:
-        btn.draw(screen)
-
-    draw_dice(screen, width, height, 'boss', boss_dice)
-    draw_dice(screen, width, height, 'player', player_dice)
+from combatScreen import combatScreen
 
 
 def main():
@@ -107,100 +20,16 @@ def main():
 
     sprites = load_sprites()
 
-    player_dice_bag = DiceBag([
-        Die(range(1,21), sprites['d20_trans']),
-        Die(range(1, 13), sprites['d12_trans']),
-        Die(range(1, 11), sprites['d10_trans']),
-        Die(range(1, 9), sprites['d8_trans']),
-        Die(range(1, 7), sprites['d6_trans']),
-        Die(range(1, 5), sprites['d4_trans']),
-        Die(range(1,21), sprites['d20_trans']),
-        Die(range(1, 13), sprites['d12_trans']),
-        Die(range(1, 11), sprites['d10_trans']),
-        Die(range(1, 9), sprites['d8_trans']),
-        Die(range(1, 7), sprites['d6_trans']),
-        Die(range(1, 5), sprites['d4_trans']),
-        Die(range(1,21), sprites['d20_trans']),
-        Die(range(1, 13), sprites['d12_trans']),
-        Die(range(1, 11), sprites['d10_trans']),
-        Die(range(1, 9), sprites['d8_trans']),
-        Die(range(1, 7), sprites['d6_trans']),
-        Die(range(1, 5), sprites['d4_trans']),
-    ], sprites['dice_bag'])
-    player = Actor(
-        'player',
-        sprites['art_boss'],
-        sprites['dice_box_player'],
-        player_dice_bag,
-        [
-            Attack([(3, 20)]),
-            Attack([(1, 12)]),
-            Attack([(2, 10)]),
-            Attack([(2, 8)]),
-            Attack([(3, 6)]),
-        ],
-    )
-
-    boss_dice_bag = DiceBag([
-        Die(range(1,21), sprites['d20_white']),
-        Die(range(1, 13), sprites['d12_white']),
-        Die(range(1, 11), sprites['d10_white']),
-        Die(range(1, 9), sprites['d8_white']),
-        Die(range(1, 7), sprites['d6_white']),
-        Die(range(1, 5), sprites['d4_white']),
-        Die(range(1,21), sprites['d20_white']),
-        Die(range(1, 13), sprites['d12_white']),
-        Die(range(1, 11), sprites['d10_white']),
-        Die(range(1, 9), sprites['d8_white']),
-        Die(range(1, 7), sprites['d6_white']),
-        Die(range(1, 5), sprites['d4_white']),
-        Die(range(1,21), sprites['d20_white']),
-        Die(range(1, 13), sprites['d12_white']),
-        Die(range(1, 11), sprites['d10_white']),
-        Die(range(1, 9), sprites['d8_white']),
-        Die(range(1, 7), sprites['d6_white']),
-        Die(range(1, 5), sprites['d4_white']),
-    ], sprites['dice_bag'])
-    boss = Actor(
-        'boss',
-        sprites['art_boss'],
-        sprites['dice_box_boss'],
-        boss_dice_bag,
-        [
-            Attack([(2, 8), (3, 6)]),
-            Attack([(3, 20)]),
-        ],
-    )
-
-    player_total, boss_total, player_dice, boss_dice = take_turn(player, boss)
-
-    buttons = []
-
-    x_mult = .2
-    for i in range(len(player.attacks)):
-        button = Button(
-            f"{player.attacks[i]}",
-            (width * x_mult, height * .8),
-            (width * .1, height * .1),
-            sprites['button_attack'],
-            'attack',
-            value=i
-        )
-        buttons.append(button)
-        x_mult += .125
+    combat_screen = combatScreen(screen, width, height, sprites)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                pos = pygame.mouse.get_pos()
-                for btn in buttons:
-                    if btn.inside(pos) and btn.kind == 'attack':
-                        player_total, boss_total, player_dice, boss_dice = take_turn(player, boss, atkA=btn.value)
-                        print(f"player = {player.dice_bag.size()}, boss = {boss.dice_bag.size()}")
-                        break
+            else:
+                combat_screen.on_event(event)
+                
 
         keys = pygame.key.get_pressed()
 
@@ -209,8 +38,7 @@ def main():
 
         screen.fill((0, 0, 0))
 
-        render_combat(screen, width, height, buttons, player, boss,
-                      player_total, boss_total, player_dice, boss_dice)
+        combat_screen.draw()
 
         pygame.display.update()
         pygame.display.flip()

858af18802e4ccf0fea6a647b74a5ac53c31fcde
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-17 10:06:30 -0500

    put sprite loading in a more appropriate location

--- src/sprites.py
+++ src/sprites.py
@@ -21,3 +21,38 @@ class Sprite:
         if scale:
             sprite = pygame.transform.scale(sprite, scale)
         screen.blit(sprite, (x, y))
+
+
+def load_sprites():
+    sprites = {}
+
+    dice_sprite_sheets = [
+        ('trans', 'assets/dice/Small/small_dice_pride_trans.png'),
+        ('white', 'assets/dice/Small/small_dice.png')
+    ]
+    dice_sprites_positions = [
+        ('d20', (0, 0), (128, 128)),
+        ('d12', (128, 0), (128, 128)),
+        ('d10', (256, 0), (128, 128)),
+        # some of these are definitely off
+        ('d8', (500, 0), (100, 128)),
+        ('d6', (620, 0), (120, 128)),
+        ('d4', (728, 0), (100, 128))
+    ]
+
+    for variant, path in dice_sprite_sheets:
+        sheet = SpriteSheet(path)
+        for die, pos, size in dice_sprites_positions:
+            sprites[f"{die}_{variant}"] = Sprite(sheet, pos, size)
+        
+    dice_box = Sprite(SpriteSheet('assets/ui/PNG/panel_brown.png'), (0, 0), (100, 100))
+    sprites['dice_box_player'] = dice_box
+    sprites['dice_box_boss'] = dice_box
+
+    inventory = SpriteSheet('assets/inventory/sprites.png')
+    sprites['art_boss'] = Sprite(inventory, (16 * 5, 16 * 7), (16, 16))
+    sprites['dice_bag'] = Sprite(inventory, (64, 16), (16, 16))
+
+    sprites['button_attack'] = Sprite(SpriteSheet('assets/ui/PNG/buttonSquare_blue.png'), (0, 0), (45, 49))
+
+    return sprites

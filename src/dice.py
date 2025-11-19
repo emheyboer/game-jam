1ea8662b0d8cb22dc6a3ecfbaf9941a1f75334d0
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-19 09:29:17 -0500

    fix naming mistake

--- src/dice.py
+++ src/dice.py
@@ -9,7 +9,7 @@ class Die:
         self.last_roll = 0
 
     def __str__(self) -> str:
-        return f"{self.sides}-sided die"
+        return f"{self.sides}-sided {type(self).__name__}"
     
     def __repr__(self) -> str:
         return str(self)
@@ -53,12 +53,12 @@ class Die:
             'd6_inverted': InvertedDie(6, sprites['d6_inverted']),
             'd4_inverted': InvertedDie(4, sprites['d4_inverted']),
 
-            'd20_pink_blue': PinkBlue(20, sprites['d20_pink_blue']),
-            'd12_pink_blue': PinkBlue(12, sprites['d12_pink_blue']),
-            'd10_pink_blue': PinkBlue(10, sprites['d10_pink_blue']),
-            'd8_pink_blue': PinkBlue(8, sprites['d8_pink_blue']),
-            'd6_pink_blue': PinkBlue(6, sprites['d6_pink_blue']),
-            'd4_pink_blue': PinkBlue(4, sprites['d4_pink_blue']),
+            'd20_pink_blue': PinkBlueDie(20, sprites['d20_pink_blue']),
+            'd12_pink_blue': PinkBlueDie(12, sprites['d12_pink_blue']),
+            'd10_pink_blue': PinkBlueDie(10, sprites['d10_pink_blue']),
+            'd8_pink_blue': PinkBlueDie(8, sprites['d8_pink_blue']),
+            'd6_pink_blue': PinkBlueDie(6, sprites['d6_pink_blue']),
+            'd4_pink_blue': PinkBlueDie(4, sprites['d4_pink_blue']),
 
             # 'd20_agender': WhiteDie(20, sprites['d20_agender']),
             # 'd12_agender': WhiteDie(12, sprites['d12_agender']),
@@ -180,7 +180,7 @@ class InvertedDie(Die):
     pass
 
 
-class PinkBlue(Die):
+class PinkBlueDie(Die):
     def roll(self) -> int:
         a = random.randint(1, self.sides)
         b = random.randint(1, self.sides)
